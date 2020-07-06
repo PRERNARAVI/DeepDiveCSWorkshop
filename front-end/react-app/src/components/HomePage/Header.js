f775e054116b6d9eddce8d0a9465759b674be623
Kassandra Flores <t-kaflor@microsoft.com>
2020-07-06 16:28:54 -0700

    header component class

---
+++ front-end/react-app/src/components/HomePage/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Row, Col, Container} from 'react-bootstrap';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Container>
+                    Header Content
+                </Container>
+            </div>
+        )
+    }
+}
