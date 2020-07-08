d086b7478648faf35d7bc6e979584567f57f486c
Lavanya Chellam <lavanyachellam3@gmail.com>
2020-07-08 11:38:06 -0400

    one working image on the fearure page

--- front-end/react-app/src/components/HomePage/Featured.js
+++ front-end/react-app/src/components/HomePage/Featured.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
-import {Row, Col, Container} from 'react-bootstrap';
+import {Row, Container} from 'react-bootstrap';
+import './imageCSS.css';
 
 export default class Featured extends Component {
     render() {
         return (
             <div>
                 <Container>
-                    Featured Content
+                    <img className = "fixedImage" src={require('./pic1.jpeg')}/>
+                     Featured Content
                 </Container>
             </div>
         )
--- front-end/react-app/src/components/HomePage/HomePage.js
+++ front-end/react-app/src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Container} from 'react-bootstrap';
 import Featured from './Featured';
 import AboutUs from './AboutUs';
 import { Nav, NavItem, NavLink } from 'reactstrap';
