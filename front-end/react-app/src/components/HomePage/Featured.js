2e30813e6ec27c45c20beba19375d0656f0003a4
Lavanya Chellam <lavanyachellam3@gmail.com>
2020-07-08 12:03:06 -0400

    text on image and basic link

--- front-end/react-app/src/components/HomePage/Featured.js
+++ front-end/react-app/src/components/HomePage/Featured.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {Row, Container} from 'react-bootstrap';
 import './imageCSS.css';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 export default class Featured extends Component {
     render() {
         return (
-            <div>
+            <div class = "container">
                 <Container>
                     <img className = "fixedImage" src={require('./pic1.jpeg')}/>
                      Featured Content
+                     <a class = "top-right" color="#0d2b68"  font="bold" href="/schedule">CLICK ME</a>
+                     <div class="bottom-left">Text about the Featured Content.</div>
+
                 </Container>
             </div>
         )
