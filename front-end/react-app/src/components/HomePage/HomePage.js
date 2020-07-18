c1a09b36662731acf0f1ed78ea6518f4aa25f52e
pravi34 <prernaravi@gatech.edu>
2020-07-18 16:56:57 -0700

    Fixed page formattting

--- front-end/react-app/src/components/HomePage/HomePage.js
+++ front-end/react-app/src/components/HomePage/HomePage.js
@@ -53,15 +53,19 @@ export default class HomePage extends Component {
                                         <NavLink onClick={(e)=>this.togglePanel(e)} className="header" 
                                         href="#">
                                             <a role="button" data-toggle="collapse" data-parent="#accordion" 
-                                            aria-expanded="true" aria-controls="collapseOne">
-                                        Topics <i id="icon" class="fas fa-angle-down" ></i>
+                                            aria-expanded="true" aria-controls="collapseOne" >
+                                        Weeks <i id="icon" class="fas fa-angle-down" ></i>
                                         </a></NavLink>
                                         {this.state.open ? (
                                         <div className="content">
-                                        <li>Topic 1</li>
-                                        <li>Topic 2</li>
-                                        <li>Topic 3</li>
-                                        <li>Topic 4</li>
+                                        <li className="list">Week 1</li>
+                                        <li className ="list">Week 2</li>
+                                        <li className ="list">Week 3</li>
+                                        <li className ="list">Week 4</li>
+                                        <li className="list">Week 5</li>
+                                        <li className ="list">Week 6</li>
+                                        <li className ="list">Week 7</li>
+                                        <li className ="list">Week 8</li>
                                         </div>
                                         ) : null}
                                     </div>
