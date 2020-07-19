d9ab16f1d9e8899388855a9e576d96bea6f3e39f
pravi34 <prernaravi@gatech.edu>
2020-07-18 23:58:09 -0700

    Added content for week 2

--- front-end/react-app/src/App.js
+++ front-end/react-app/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage';
 import './components/HomePage/styles.css'
+import WeekTwo from './components/HomePage/WeekTwo/WeekTwo';
 
 function App() {
   return (<Router>
@@ -11,6 +12,7 @@ function App() {
       <Switch>
           <Route exact path='/' component={HomePage} />     
           <Route path="/schedule" component={HomePage} /> 
+          <Route path='/week2' component={WeekTwo} />
           
       </Switch>
     </div>
---
+++ front-end/react-app/src/components/HomePage/WeekTwo/Content.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Row, Col, Container} from 'react-bootstrap';
+import Featured from '../Featured';
+import AboutUs from '../AboutUs';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import Header from '../Header';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+export default class Content extends Component {
+    render() {
+        return (
+            <div>
+                <Container className="about-us-comtainer" >
+                    <h3 className="about-us-heading">Welcome to Week 2!</h3>
+                    <div className = "about-us-text">
+                    In this week, we will be introducing you to <b><u>Arrays and Strings</u></b> on the Technical Side.
+                    We will also be providing tips on <b><u>Proactive Communication</u></b> to train you for behavioral interviews.
+                    Given below is a link to the recorded workshop. You can also view and access the material from 
+                    the workshop following that. Finally, we have also allocated a section below to go over assigned coding 
+                    problems pertaining to arrays and strings, to help deepen your understanding for these critical concepts.
+                    We have tried to pick out those questions and patterns that commonly appear in technical interviews.
+                    </div>
+                    <div className = "about-us-text">
+                    If you have any questions, feel free to reach out to us. Hope this helps!
+                    </div>
+                    <h4 className="about-us-heading">Workshop Recordings</h4>
+                    <div className = "about-us-text">
+                    You can find the recorded video from the workshop below!
+                    </div>
+                    <div>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/jX6kn9_U8qk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true" width="950px" height="565px"></iframe>
+                    </div>
+
+
+                    <div>
+                    <iframe className = "soft-skill-ppt" src="https://gtvault-my.sharepoint.com/personal/pravi34_gatech_edu/_layouts/15/Doc.aspx?sourcedoc={f1292387-001a-4d66-8428-a617574ad792}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="950px" height="565px" >This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+                    </div>
+                 </Container>
+
+                
+                
+            </div>
+        
+        )
+    }
+} 
