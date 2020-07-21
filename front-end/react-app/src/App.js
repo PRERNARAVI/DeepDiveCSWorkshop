7b658f352d402fa081a934f2889f3e82730b34fc
kassandrafloressc54@gmail.com <Redr0se.54>
2020-07-21 09:34:38 -0700

    regrence new schedule page

--- front-end/react-app/src/App.js
+++ front-end/react-app/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage';
-import './components/HomePage/styles.css'
+import './components/HomePage/styles.css';
+import SchedulePage from './components/HomePage/SchedulePage';
 
 function App() {
   return (<Router>
     <div className="App">
       <Switch>
           <Route exact path='/' component={HomePage} />     
-          <Route path="/schedule" component={HomePage} /> 
-          
+          <Route path="/schedule" component={SchedulePage} /> 
       </Switch>
     </div>
   </Router>   
