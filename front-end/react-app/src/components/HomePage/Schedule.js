d600ffb5bd746e101eee90182ab7ffb0d6758544
kassandrafloressc54@gmail.com <Redr0se.54>
2020-07-21 09:34:26 -0700

    file extension change

--- front-end/react-app/src/components/HomePage/Schedule.js
+++ front-end/react-app/src/components/HomePage/Schedule.js
@@ -9,7 +9,11 @@ import {
   {
       return(
           <Container id = "CardDeck-Container">
-            <FullWidthTabs/>
+            <Row>
+                <Col xs>First, but unordered</Col>
+                <Col xs={{ order: 12 }}>Second, but last</Col>
+                <Col xs={{ order: 1 }}>Third, but second</Col>
+            </Row>
           </Container>
         
           
