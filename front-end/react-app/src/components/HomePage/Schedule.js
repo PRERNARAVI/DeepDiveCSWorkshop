476af202acaf2fe58f6d7bb2ba55a15b2d0ff5b1
kassandrafloressc54@gmail.com <Redr0se.54>
2020-07-21 20:09:42 -0700

    tiles and css changes

--- front-end/react-app/src/components/HomePage/Schedule.js
+++ front-end/react-app/src/components/HomePage/Schedule.js
@@ -4,16 +4,27 @@ import {
     Card, Button, CardImg, CardTitle, CardText, CardGroup, CardDeck,
     CardSubtitle, CardBody
   } from 'reactstrap';
+import card_cover from '../../assets/card_cover.jpg'
+
+  var divStyle = {
+    //backgroundImage: 'url("https://assets.leetcode.com/explore/cards/30-day-coding-challenge/img")',
+    backgroundImage: 'url(' + card_cover + ')',
+    backgroundSize: 'cover',
+    backgroundPosition: 'right center',
+    // WebkitTransition: 'all', // note the capital 'W' here
+    msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+  };
 
   const WorkshopWeekCards = (props) =>
   {
+      
       return(
           <div  id = "Week-Tile-Container">
             <Row>
                 <Col>
                     <div className = "week-tile">
                         <div className = "week-tile-muted-green">
-                            <div className = 'week-tile-content'>
+                            <div className = 'week-tile-content' >
                                 <div className = 'week-tile-title'>
                                     Week One
                                 </div>
@@ -28,8 +39,156 @@ import {
                         </div>
                     </div>
                 </Col>
-                <Col >Second, but last</Col>
-                <Col >Third, but second</Col>
+                <Col >
+                    <div className = "week-tile">
+                        <div className = "week-tile-muted-blue">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week Two
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Strings 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col >
+                <div className = "week-tile">
+                        <div className = "week-tile-muted-red">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week Three
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Strings 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+            <div className = "week-tile-seperator"></div>
+            <Row>
+                <Col>
+                    <div className = "week-tile">
+                        <div className = "week-tile-muted-yellow">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week One
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Arrays and Sorting 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col >
+                    <div className = "week-tile">
+                        <div className = "week-tile-muted-brown">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week Two
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Strings 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col >
+                <div className = "week-tile">
+                        <div className = "week-tile-muted-green">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week Three
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Strings 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+            <div className = "week-tile-seperator"></div>
+            <Row>
+                <Col>
+                    <div className = "week-tile">
+                        <div className = "week-tile-muted-blue">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week One
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Arrays and Sorting 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col >
+                    <div className = "week-tile">
+                        <div className = "week-tile-muted-red">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week Two
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Strings 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col >
+                <div className = "week-tile">
+                        <div className = "week-tile-muted-brown">
+                            <div className = 'week-tile-content' >
+                                <div className = 'week-tile-title'>
+                                    Week Three
+                                </div>
+                                <div className = 'week-tile-body'>
+                                    Strings 
+                                    <br/>
+                                    and
+                                    <br/>
+                                    Proactive Communication
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
             </Row>
           </div>
 
