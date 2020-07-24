4291f3668521c95e97dd1e03f3200deca44b39ab
pravi34 <prernaravi@gatech.edu>
2020-07-24 16:01:27 -0700

    Browser router added

--- front-end/react-app/src/index.js
+++ front-end/react-app/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 export { default as Fade } from './components/HomePage/Fade';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>  
+      <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
